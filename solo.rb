33db897b969e8a7b31052c42b9bb8cadab7d2dbb
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-02-02 19:20:11 +0800

    fix chef solo cache path, avoid checksums put with files

--- solo.rb
+++ solo.rb
@@ -2,9 +2,9 @@ top_dir = File.dirname(File.expand_path(__FILE__))
 
 node_name 'solo'
 cache_type 'BasicFile'
-cache_options :path => File.join(top_dir, '.chef/solo/cache')
-file_cache_path File.join(top_dir, '.chef/solo/cache')
-file_backup_path File.join(top_dir, '.chef/solo/backup')
+cache_options :path => File.join(top_dir, '.chef/solo/cache/checksums')
+file_cache_path File.join(top_dir, '.chef/solo/cache/files')
+file_backup_path File.join(top_dir, '.chef/solo/cache/backup')
 cookbook_path File.join(top_dir, 'cookbooks')
 role_path File.join(top_dir, 'roles')
 client_key File.join(top_dir, '.chef/insecure_client_key.pem')
