61ee9deac571d03892d9416fb11375a03d41934d
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-06-05 22:26:47 +0800

    renamed: servers/vagrant-vm-sample.rb -> servers/vagrant.rb

--- servers/vagrant.rb
+++ servers/vagrant.rb
@@ -3,6 +3,6 @@ host '127.0.0.1'
 port 2222
 ssh_options(:keys => '~/.vagrant.d/insecure_private_key')
 
-node.set[:name] = 'localhost-sample'
+node.set[:name] = 'vagrant'
 
 run_list ['recipe[hello-world]']
